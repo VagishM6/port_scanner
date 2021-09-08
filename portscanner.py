b79c35e7445a6f54a4abeeb1c077d024cc01e460
VagishM6 <iamvagish6@gmail.com>
2021-09-08 19:56:03 +0530

    adding port scanner

---
+++ portscanner.py
@@ -0,0 +1,31 @@
+import socket
+from IPy import IP
+
+
+# checking if the ip address is numeric, else converting the domain to ip address
+def check_ip(ip):
+    try:
+        IP(ip)
+        return ip
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+
+# function to scan the port
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print('[+] Port ' + str(port) + ' is Open')
+    except:
+        print('[-] Port ' + str(port) + ' is Closed')
+
+
+# specifying the ipaddress
+ipaddress = input('[+] Enter Target to Scan: ')
+converted_ip = check_ip(ipaddress)
+
+# range of ports to scan
+for port in range(75, 85):
+    scan_port(converted_ip, port)
